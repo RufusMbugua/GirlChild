f288cf0dba7a582529567622ad4534475da5b126
Mbugua <mbuguarufus@gmail.com>
2013-03-29 06:03:58 +0300

    Initial Commit

---
+++ Controllers/HomeController.vb
@@ -0,0 +1,27 @@
+﻿Public Class HomeController
+    Inherits System.Web.Mvc.Controller
+
+    Function Index() As ActionResult
+        ViewData("Message") = "Modify this template to jump-start your ASP.NET MVC application."
+
+        Return View()
+    End Function
+
+    Function About() As ActionResult
+        ViewData("Message") = "Your app description page."
+
+        Return View()
+    End Function
+
+    Function Contact() As ActionResult
+        ViewData("Message") = "Your contact page."
+
+        Return View()
+    End Function
+
+    Function Report() As ActionResult
+        ViewData("Message") = "Fill in a report."
+
+        Return View()
+    End Function
+End Class
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="GirlChild.MvcApplication" Language="VB" %>
